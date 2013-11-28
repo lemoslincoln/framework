7cef45be5e8f676835020874907135640c4869c3
Lincoln Lemos <lemoslincoln@gmail.com>
2013-11-28 12:24:42 -0200

    Add meta tag viewport

--- header.php
+++ header.php
@@ -12,6 +12,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 
 <link rel="profile" href="http://gmpg.org/xfn/11" />

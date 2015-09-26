ce7c8e0da41552c17926d710474313bfbe94f32b
Lincoln de Sousa Cunha Lemos <lincoln@bigodesign.com.br>
2015-09-26 03:21:22 -0300

    _social-links.php

--- header.php
+++ header.php
@@ -68,17 +68,7 @@
 				</section>
 				
 				<section class="col-sm-2">
-					<?php if( have_rows('sociais_info','option') ): ?>
-						<ul class="redes-sociais">
-							<?php  while ( have_rows('sociais_info','option') ) : the_row(); ?>
-								<li>
-									<a href="<?php the_sub_field('url_info'); ?>" target="_blank">
-										<?php the_sub_field('icone_info'); ?>
-									</a>
-								</li>
-							<?php endwhile; ?>
-						</ul>
-					<?php endif; ?>
+					<?php get_template_part('partials/_social-links.php') ?>
 				</section>
 			</section>
 		</section>

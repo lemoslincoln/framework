56f03e8a862eb16fcf1f3723386288b75c5e87f5
Lincoln de Sousa Cunha Lemos <lincoln@bigodesign.com.br>
2015-10-23 00:29:01 -0200

    Add function clear_url

--- functions/funcoes.php
+++ functions/funcoes.php
@@ -35,3 +35,22 @@ function mascara_string($mascara,$string) {
    }
    return $mascara;
 }
+
+
+function clear_url($input) {
+  // in case scheme relative URI is passed, e.g., //www.google.com/
+  $input = trim($input, '/');
+
+  // If scheme not included, prepend it
+  if (!preg_match('#^http(s)?://#', $input)) {
+      $input = 'http://' . $input;
+  }
+
+  $urlParts = parse_url($input);
+
+  // remove www
+  $domain = preg_replace('/^www\./', '', $urlParts['host']);
+
+  return $domain;
+
+}

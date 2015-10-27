b76345f6225b3a50e2d9ddd81ae1faec4d941ca2
Lincoln de Sousa Cunha Lemos <lincoln@bigodesign.com.br>
2015-10-27 17:23:01 -0200

    newsletter

---
+++ partials/_newsletter.php
@@ -0,0 +1,8 @@
+<div class="row">
+	<div class="col-sm-4">
+		<img src="<?php images_url('newsletter.png') ?>" alt="Receba novidades em seu e-mail" class="img-responsive">
+	</div>
+	<div class="col-sm-8">
+		<?php echo do_shortcode('[simplenewsletter]'); ?>
+	</div>
+</div>

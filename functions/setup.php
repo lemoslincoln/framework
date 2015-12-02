e07f4870a6daa70904f4852497394664bbba06b3
Lincoln de Sousa Cunha Lemos <lincoln@bigodesign.com.br>
2015-12-02 02:52:04 -0200

    Adicionando páginas automáticas

--- functions/setup.php
+++ functions/setup.php
@@ -282,3 +282,48 @@ function bigo_get_images($size = 'thumbnail') {
   global $post;
   return get_children( array('post_parent' => $post->ID, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => 'ASC', 'orderby' => 'menu_order') );
 }
+
+
+
+
+/* Cria páginas ao instalar o tema */
+/* ----------------------------------------- */
+// programmatically create some basic pages, and then set Home and Blog
+// setup a function to check if these pages exist
+function the_slug_exists($post_name) {
+	global $wpdb;
+	if($wpdb->get_row("SELECT post_name FROM wp_posts WHERE post_name = '" . $post_name . "'", 'ARRAY_A')) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+$paginas = [
+	// [Title, Content, 'Slug']
+	['Home', '', 'home'],
+	['Blog', '', 'blog'],
+	['Institucional', '', 'institucional'],
+	['Fale Conosco', '', 'fale-conosco'],
+
+];
+// Cria as páginas
+if (isset($_GET['activated']) && is_admin()){
+		foreach ($paginas as $pagina) {
+			$page_check = get_page_by_title($pagina[0]);
+			if(!isset($page_check->ID) && !the_slug_exists($pagina[2])){
+			    $newPageId = wp_insert_post(array(
+			    	'post_type' => 'page',
+			    	'post_title' => $pagina[0],
+			    	'post_content' => $pagina[1],
+			    	'post_status' => 'publish',
+			    	'post_author' => 1,
+			    	'post_slug' => $pagina[2]
+			    ));
+			    if ($pagina[0] == 'Home') { update_option( 'page_on_front', $newPageId ); update_option( 'show_on_front', 'page' ); }
+			    if ($pagina[0] == 'Blog') { update_option( 'page_for_posts', $newPageId ); }
+			}	
+		}
+}
+
+/* ----------------------------------------- Cria páginas ao instalar o tema */		

1debcb00d5d180b07b5b0175ad06815b0a0dd921
Lincoln de Sousa Cunha Lemos <lincoln@bigodesign.com.br>
2015-12-15 12:09:15 -0200

    Fix bootstrap tabs with google maps

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,157 +1,177 @@
-jQuery(document).ready(function($){ 
-  // Google maps
+(function($) {
+
+/*
+*  new_map
+*
+*  This function will render a Google Map onto the selected jQuery element
+*
+*  @type  function
+*  @date  8/11/2013
+*  @since 4.3.0
+*
+*  @param $el (jQuery element)
+*  @return  n/a
+*/
+
+function new_map( $el ) {
   
-  /*
-  *  render_map
-  *
-  *  This function will render a Google Map onto the selected jQuery element
-  *
-  *  @type  function
-  *  @date  8/11/2013
-  *  @since 4.3.0
-  *
-  *  @param $el (jQuery element)
-  *  @return  n/a
-  */
+  // var
+  var $markers = $el.find('.marker');
+  
+  
+  // vars
+  var args = {
+    zoom    : 16,
+    center    : new google.maps.LatLng(0, 0),
+    mapTypeId : google.maps.MapTypeId.ROADMAP
+  };
+  
+  
+  // create map           
+  var map = new google.maps.Map( $el[0], args);
+  
+  
+  // add a markers reference
+  map.markers = [];
+  
+  
+  // add markers
+  $markers.each(function(){
+    
+      add_marker( $(this), map );
+    
+  });
+  
+  
+  // center map
+  center_map( map );
+  
+  
+  // return
+  return map;
+  
+}
 
-  function render_map( $el ) {
+/*
+*  add_marker
+*
+*  This function will add a marker to the selected Google Map
+*
+*  @type  function
+*  @date  8/11/2013
+*  @since 4.3.0
+*
+*  @param $marker (jQuery element)
+*  @param map (Google Map object)
+*  @return  n/a
+*/
 
-    // var
-    var $markers = $el.find('.marker');
+function add_marker( $marker, map ) {
 
-    // vars
-    var args = {
-      zoom    : 16,
-      center    : new google.maps.LatLng(0, 0),
-      mapTypeId : google.maps.MapTypeId.ROADMAP
-    };
+  // var
+  var latlng = new google.maps.LatLng( $marker.attr('data-lat'), $marker.attr('data-lng') );
 
-    // create map           
-    var map = new google.maps.Map( $el[0], args);
+  // create marker
+  var marker = new google.maps.Marker({
+    position  : latlng,
+    map     : map
+  });
 
-    // add a markers reference
-    map.markers = [];
+  // add to array
+  map.markers.push( marker );
 
-    // add markers
-    $markers.each(function(){
-
-        add_marker( $(this), map );
-
+  // if marker contains HTML, add it to an infoWindow
+  if( $marker.html() )
+  {
+    // create info window
+    var infowindow = new google.maps.InfoWindow({
+      content   : $marker.html()
     });
 
-    // center map
-    center_map( map );
+    // show info window when marker is clicked
+    google.maps.event.addListener(marker, 'click', function() {
 
+      infowindow.open( map, marker );
+
+    });
   }
 
-  /*
-  *  add_marker
-  *
-  *  This function will add a marker to the selected Google Map
-  *
-  *  @type  function
-  *  @date  8/11/2013
-  *  @since 4.3.0
-  *
-  *  @param $marker (jQuery element)
-  *  @param map (Google Map object)
-  *  @return  n/a
-  */
+}
 
-  function add_marker( $marker, map ) {
+/*
+*  center_map
+*
+*  This function will center the map, showing all markers attached to this map
+*
+*  @type  function
+*  @date  8/11/2013
+*  @since 4.3.0
+*
+*  @param map (Google Map object)
+*  @return  n/a
+*/
 
-    // var
-    var latlng = new google.maps.LatLng( $marker.attr('data-lat'), $marker.attr('data-lng') );
+function center_map( map ) {
 
-    // create marker
-    var marker = new google.maps.Marker({
-      position  : latlng,
-      map     : map
-    });
+  // vars
+  var bounds = new google.maps.LatLngBounds();
 
-    // add to array
-    map.markers.push( marker );
+  // loop through all markers and create bounds
+  $.each( map.markers, function( i, marker ){
 
-    // if marker contains HTML, add it to an infoWindow
-    if( $marker.html() )
-    {
-      // create info window
-      var infowindow = new google.maps.InfoWindow({
-        content   : $marker.html()
-      });
+    var latlng = new google.maps.LatLng( marker.position.lat(), marker.position.lng() );
 
-      // show info window when marker is clicked
-      google.maps.event.addListener(marker, 'click', function() {
-
-        infowindow.open( map, marker );
-
-      });
-    }
-
-  }
-
-  /*
-  *  center_map
-  *
-  *  This function will center the map, showing all markers attached to this map
-  *
-  *  @type  function
-  *  @date  8/11/2013
-  *  @since 4.3.0
-  *
-  *  @param map (Google Map object)
-  *  @return  n/a
-  */
-
-  function center_map( map ) {
-
-    // vars
-    var bounds = new google.maps.LatLngBounds();
-
-    // loop through all markers and create bounds
-    $.each( map.markers, function( i, marker ){
-
-      var latlng = new google.maps.LatLng( marker.position.lat(), marker.position.lng() );
-
-      bounds.extend( latlng );
-
-    });
-
-    // only 1 marker?
-    if( map.markers.length == 1 )
-    {
-      // set center of map
-        map.setCenter( bounds.getCenter() );
-        map.setZoom( 16 );
-    }
-    else
-    {
-      // fit to bounds
-      map.fitBounds( bounds );
-    }
-
-  }
-
-  /*
-  *  document ready
-  *
-  *  This function will render each map when the document is ready (page has loaded)
-  *
-  *  @type  function
-  *  @date  8/11/2013
-  *  @since 5.0.0
-  *
-  *  @param n/a
-  *  @return  n/a
-  */
-
-  $(document).ready(function(){
-
-    $('.acf-map').each(function(){
-
-      render_map( $(this) );
-
-    });
+    bounds.extend( latlng );
 
   });
+
+  // only 1 marker?
+  if( map.markers.length == 1 )
+  {
+    // set center of map
+      map.setCenter( bounds.getCenter() );
+      map.setZoom( 16 );
+  }
+  else
+  {
+    // fit to bounds
+    map.fitBounds( bounds );
+  }
+
+}
+
+/*
+*  document ready
+*
+*  This function will render each map when the document is ready (page has loaded)
+*
+*  @type  function
+*  @date  8/11/2013
+*  @since 5.0.0
+*
+*  @param n/a
+*  @return  n/a
+*/
+// global var
+var maps = {};
+
+$(document).ready(function(){
+  
+  $('.acf-map').each(function(){
+    var contentID = $(this).attr('href');   
+    maps.contentID = new_map( $(this) );
+    console.log(maps.contentID);
+  });
+
+  $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {      
+      // Linkão comedor de buceta que fez essa merda funcionar!!
+      var contentID = $(this).attr('href'); 
+      var map = maps.contentID;
+      var center=map.getCenter();
+      google.maps.event.trigger(map, "resize");
+      map.setCenter(center);
+  });
+
 });
+
+})(jQuery);

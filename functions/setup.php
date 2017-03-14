16a3fcac8b37927bc596a15fcc1f7ddb9c212585
Lincoln de Sousa Cunha Lemos <lemoslincoln@gmail.com>
2017-03-14 15:38:21 -0300

    Remove default emojis

--- functions/setup.php
+++ functions/setup.php
@@ -259,6 +259,9 @@ function angolanos_add_fancybox() {
 }
 add_action( 'wp_enqueue_scripts', 'angolanos_add_fancybox' );
 
+// Remove default Emojis for Wordpress
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
 
 /* Carrega Arquivos CSS do tema */
 /* ----------------------------------------- */
